9697593ce9c25db940f0ef833ead1b6d07f59f02
chamithdilhara41 <chamithdilhara41@gmail.com>
2024-03-13 12:06:46 +0530

    first commit

---
+++ src/main/java/lk/ijse/dep/service/BoardImpl.java
@@ -0,0 +1,52 @@
+package lk.ijse.dep.service;
+
+public class BoardImpl implements Board {
+
+    private Piece[][] pieces;
+    private BoardUI boardUI;
+    public BoardImpl(BoardUI boardUI) {
+        this.boardUI = boardUI;
+
+        Piece [][] pieces = new Piece[NUM_OF_COLS][NUM_OF_ROWS];
+
+        for (int i = 0; i <pieces.length; i++) {
+            for (int j = 0; j < pieces[i].length; j++) {
+                pieces[i][j]=Piece.EMPTY;
+            }
+        }
+    }
+    @Override
+    public BoardUI getBoardUI() {
+        return boardUI;
+    }
+
+    @Override
+    public int findNextAvailableSpot(int col) {
+        for (int i = 0; i < pieces[col].length; i++) {
+            if (pieces[col][i] == Piece.EMPTY) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public boolean isLegalMove(int col) {
+        return false;
+    }
+
+    @Override
+    public boolean existLegalMoves() {
+        return false;
+    }
+
+    @Override
+    public void updateMove(int col, Piece move) {
+
+    }
+
+    @Override
+    public Winner findWinner() {
+        return null;
+    }
+}
